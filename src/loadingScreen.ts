2a26d42ea2ecd0bb946552daf5163047a3d5f34e
thelinus1997 <thelinus1997@gmail.com>
2024-01-18 13:41:21 +0100

    Fixat basics för loadingscreen

--- src/loadingScreen.ts
+++ src/loadingScreen.ts
@@ -1,4 +1,12 @@
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const centerCont: HTMLDivElement = document.createElement("div");
+centerCont.classList.add("center");
 const imgElement: HTMLImageElement = document.createElement("img");
 imgElement.src = "../public/logo.svg";
-app.append(imgElement);
+imgElement.classList.add("logo");
+const title: HTMLElement = document.createElement("h1");
+title.innerText = "INTERVAL";
+const subTitle: HTMLElement = document.createElement("p");
+subTitle.innerText = "For all your timing needs";
+centerCont.append(imgElement, title, subTitle);
+app.append(centerCont);
