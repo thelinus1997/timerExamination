ec0a7dd7864c9e692fc68ca8ad8cab468bfaf34e
thelinus1997 <thelinus1997@gmail.com>
2024-01-18 09:46:28 +0100

    början på examinationsuppgiften

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+interface Todo {
+  id: number;
+  label: string;
+  description?: string;
+  done: boolean;
+}
+
+const todos: Todo[] = [
+  { id: 0, label: "buy groceries", done: true },
+  { id: 1, label: "buy eggs", done: false },
+  { id: 2, label: "sleep", done: true },
+];
+
+todos.forEach((todo) => {
+  const div = document.createElement("div");
+  div.innerHTML = `label: ${todo.label} - done: ${todo.done}`;
+  app.append(div);
+});
