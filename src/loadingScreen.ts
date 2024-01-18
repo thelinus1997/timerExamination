df53623e9c9fcb8ab7093fefef3bfa4bff6a96fb
thelinus1997 <thelinus1997@gmail.com>
2024-01-18 13:37:32 +0100

    logo

---
+++ src/loadingScreen.ts
@@ -0,0 +1,4 @@
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const imgElement: HTMLImageElement = document.createElement("img");
+imgElement.src = "../public/logo.svg";
+app.append(imgElement);
