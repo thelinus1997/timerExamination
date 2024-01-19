e0aa1b5f8d4f94244e0df93f9c2a8511f9d44b2a
thelinus1997 <thelinus1997@gmail.com>
2024-01-19 14:22:14 +0100

    timer work

--- src/setTimer.ts
+++ src/setTimer.ts
@@ -15,6 +15,31 @@ function buildPage() {
   imgCont.src = "../public/flippedLogo.svg";
   imgCont.classList.add("logo");
   logoCont.appendChild(imgCont);
-  main.append(logoCont);
+
+  const timeContainer: HTMLDivElement = document.createElement("div");
+  const arrowLeft: HTMLImageElement = document.createElement("img");
+  const arrowRight: HTMLImageElement = document.createElement("img");
+  const minuteContainer: HTMLDivElement = document.createElement("div");
+  const minuteShower: HTMLElement = document.createElement("h1");
+  const minuteText: HTMLElement = document.createElement("p");
+
+  const choiceContainer: HTMLDivElement = document.createElement("div");
+  const checkboxContainer: HTMLDivElement = document.createElement("div");
+  const textContainer: HTMLDivElement = document.createElement("div");
+
+  const checkBoxOne: HTMLInputElement = document.createElement("input");
+  checkBoxOne.type = "checkbox";
+  const checkBoxTwo: HTMLInputElement = document.createElement("input");
+  checkBoxTwo.type = "checkbox";
+
+  const textOne: HTMLElement = document.createElement("p");
+  const textTwo: HTMLElement = document.createElement("p");
+
+  checkboxContainer.append(checkBoxOne, checkBoxTwo);
+  textContainer.append(textOne, textTwo);
+  choiceContainer.append(checkboxContainer, textContainer);
+  minuteContainer.append(minuteShower, minuteText);
+  timeContainer.append(arrowLeft, minuteContainer, arrowRight);
+  main.append(logoCont, timeContainer, choiceContainer);
   app.appendChild(main);
 }
