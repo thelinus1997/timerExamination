f1a37f2a483c696fc6df32c0e3d375641527f33f
93mcqmil <mcqueen_1993@hotmail.com>
2024-01-20 15:38:02 +0100

    first Menu setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./scss/style.scss";
 import { startLoading } from "./loadingScreen";
 import "./analog";
+import "./menu";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 console.log(app);
