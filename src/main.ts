785819382a7a48d2a5e90bb4f29dc17dd62c2f1a
93mcqmil <mcqueen_1993@hotmail.com>
2024-01-20 16:40:00 +0100

    konfliklösning

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,10 @@ import "./scss/style.scss";
 import "./scss/_digitalTimer.scss"
 import { startLoading } from "./loadingScreen";
 import "./analog";
-<<<<<<< HEAD
 import "./scss/_analog.scss";
 import { setTimer } from "./setTimer";
 import { startCountdown } from "./digital";
-=======
 import "./menu";
->>>>>>> mackan
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 console.log(app);
