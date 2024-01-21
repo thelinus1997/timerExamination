67fdeb73e1d2e53b4044e9d3baf0c5df96919495
EllieBach <elisabeth.bach95@hotmail.com>
2024-01-21 17:06:53 +0100

    Visual timer ts and scss

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import "./analog";
 import "./scss/_analog.scss";
 import { setTimer } from "./setTimer";
 import { startCountdown } from "./digital";
+import { visualTimerFunc } from "./visual";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 console.log(app);
 //startLoading();
-startCountdown();
+//startCountdown();
 
+visualTimerFunc();
