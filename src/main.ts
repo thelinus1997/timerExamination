fdf7f1c748d2f5944bbdeed726c0ce1261500573
thelinus1997 <thelinus1997@gmail.com>
2024-01-24 11:53:18 +0100

    fix merge issues

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./scss/style.scss";
-import "./scss/_digitalTimer.scss"
+import "./scss/_digitalTimer.scss";
 import { startLoading } from "./loadingScreen";
 import "./analog";
 import "./scss/_analog.scss";
@@ -12,24 +12,6 @@ import { analogStart } from "./analog";
 import { alarmView } from "./alarmvy";
 import { breakView } from "./breakview";
 
-
 const app = document.querySelector<HTMLDivElement>("#app")!;
 console.log(app);
 startLoading();
-
-
-<<<<<<< HEAD
-=======
-//visualTimerFunc();
-// const timer = new Timer();
-// createMenu();
-// startLoading();
-// startCountdown();
-// startLoading();
-// startCountdown();
-//breakView(); //startCountdown();
-
-// //startLoading();
-// startCountdown();
-startLoading();
->>>>>>> 174b382b53dd94fe3c42c55004b634008e33660f
